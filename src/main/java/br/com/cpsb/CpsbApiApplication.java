c866268849f6377c8d042578353c29ef2f9530a9
Gabriel Eringer <rm99632@fiap.com.br>
2024-09-27 17:54:35 -0300

    Adding starter-web & starter-data-jpa dependencies

--- src/main/java/br/com/cpsb/CpsbApiApplication.java
+++ src/main/java/br/com/cpsb/CpsbApiApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CpsbApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(CpsbApiApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(CpsbApiApplication.class, args);
+    }
 
 }
