b09b83b66f12b3a0eef2b071dadaffba463d8d33
Gabriel Eringer <rm99632@fiap.com.br>
2024-09-27 17:49:19 -0300

    Initial commit

---
+++ src/main/java/br/com/cpsb/CpsbApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.cpsb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpsbApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpsbApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/cpsb/CpsbApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.cpsb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpsbApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
