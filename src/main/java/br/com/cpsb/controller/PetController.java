5d805b141c7aa90111ca2b2076a13cdb23d4fa06
Gabriel Eringer <rm99632@fiap.com.br>
2024-09-27 18:55:02 -0300

    Mapping lista_pets page in PetController

--- src/main/java/br/com/cpsb/controller/PetController.java
+++ src/main/java/br/com/cpsb/controller/PetController.java
@@ -1,18 +1,30 @@
 package br.com.cpsb.controller;
 
+import br.com.cpsb.model.Pet;
+import br.com.cpsb.repository.PetRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+
 @Controller
 public class PetController {
     
-    @GetMapping()
-    public ModelAndView showIndex() {
-        return new ModelAndView("index");
-    }
+    @Autowired
+    private PetRepository petRepository;
 
     //TODO Lista de pets
+    @GetMapping()
+    public ModelAndView listPets() {
+        ModelAndView mv = new ModelAndView("lista_pets");
+        
+        List<Pet> pets = petRepository.findAll();
+        mv.addObject("pets", pets);
+        
+        return mv;
+    }
 
     //TODO Cadastro de pet
 
