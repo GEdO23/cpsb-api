2336b0b3f3406f4cbbf40188513789fe5aa000f1
Gabriel Eringer <rm99632@fiap.com.br>
2024-09-27 18:54:05 -0300

    Creating Pet Repository

---
+++ src/main/java/br/com/cpsb/repository/PetRepository.java
@@ -0,0 +1,7 @@
+package br.com.cpsb.repository;
+
+import br.com.cpsb.model.Pet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PetRepository extends JpaRepository<Pet, Long> {
+}
