ffdbf6e61636d0e7d5d7206aa8838312e6f7bb8a
Gabriel Eringer <rm99632@fiap.com.br>
2024-10-02 18:30:14 -0300

    Making it so only ROLE_ADMIN can create, update and delete Pets or Races

--- src/main/java/br/com/cpsb/security/SecurityConfig.java
+++ src/main/java/br/com/cpsb/security/SecurityConfig.java
@@ -13,8 +13,15 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated())
+        http.authorizeHttpRequests((requests) -> requests.requestMatchers(
+                                "/formulario_cadastrar_pet",
+                                "/formulario_atualizar_pet/{id}",
+                                "/remover_pet/{id}",
+                                "/formulario_cadastrar_raca")
+                        .hasAuthority("ROLE_ADMIN")
+                        .anyRequest().authenticated())
                 .formLogin((form) -> form.loginPage("/login")
+                        .defaultSuccessUrl("/")
                         .failureUrl("/login?erro_login=true")
                         .permitAll())
                 .logout((logout) -> logout.logoutUrl("/logout")
@@ -23,7 +30,7 @@ public class SecurityConfig {
 
         return http.build();
     }
-    
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
