b0caa9cd52c3a2168d1a802ad2171da9dda7dd69
Gabriel Eringer <rm99632@fiap.com.br>
2024-10-01 21:18:16 -0300

    Adding SecurityConfig and functional Login

---
+++ src/main/java/br/com/cpsb/security/SecurityConfig.java
@@ -0,0 +1,25 @@
+package br.com.cpsb.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests((requests) -> requests.anyRequest().authenticated())
+                .formLogin((form) -> form.loginPage("/login")
+                        .failureUrl("/login?erro_login=true")
+                        .permitAll())
+                .logout((logout) -> logout.logoutUrl("/logout")
+                        .logoutSuccessUrl("/login?logout=true")
+                        .permitAll());
+
+        return http.build();
+    }
+
+}
